5031e520d9e23601113a396a661787890de19c9c
khizir chuwdhury <khizirmchuwdhury@gmail.com>
2022-08-19 23:00:20 -0400

    add homework3 algorithm

--- arithmetic_number.py
+++ arithmetic_number.py
@@ -1,10 +1,31 @@
 
 
-n_num = [1, 3, 5, 6, 4, 10, 2, 3]
-n = len(n_num)
-print(n_num)
-get_sum = sum(n_num)
-mean = get_sum / n
+def below_amen(arr):
+    amean = sum(arr) / len(arr)
+    result_list = []
+    i = 0
+    while i < len(arr):
+        if arr[i] < amean:
+            result_list.append(arr[i])
+        i += 1
+
+    return result_list
 
 
-print("Mean is :  " + str(mean))
+test_list = [1, 3, 5, 6, 4, 10, 2, 3]
+result_list = below_amen(test_list)
+print(result_list)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
